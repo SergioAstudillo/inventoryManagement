04edd73114890f528a1e57e14e59b51429a050bf
Sergio Astudillo <sergioastudillo@protonmail.com>
2021-11-22 20:30:40 +0100

    Initial configuration of the project. TailwindCSS config + React && TS config + Spring config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		fontFamily: {
+			main: ['Montserrat']
+		},
+		extend: {}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
+};
